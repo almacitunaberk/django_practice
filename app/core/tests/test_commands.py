c9fc2889f82f4dba95070e86588ca3dfee594e2c
Tunaberk Almaci <almacitunaberk@gmail.com>
2022-11-04 15:06:03 +0300

    Lintin issues solved

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -8,16 +8,19 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch("core.management.commands.wait_for_db.Command.check")
 class CommandTests(SimpleTestCase):
+
+
     """Test Commands"""
-    
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready"""
         patched_check.return_value = True
         call_command('wait_for_db')
         patched_check.assert_called_once_with(databases=['default'])
-    
+        
+
     @patch("time.sleep")
     def test_wait_for_delay(self, patched_sleep, patched_check):
         """Test waiting for db when getting operational error"""
@@ -26,4 +29,3 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-        
